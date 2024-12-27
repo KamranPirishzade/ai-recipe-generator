45345270c7ade66532a9a45d3b312f2195067f3c
Kamran <KamranPirishzade@gmail.com>
2024-12-26 21:51:31 -0800

    Deploy commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {
-  BrowserRouter as Router, Routes, Route,
+  HashRouter as Router, Routes, Route,
 } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
